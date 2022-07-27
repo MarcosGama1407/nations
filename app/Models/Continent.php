8f8149258f9268a8255029d3ed0a70a4739fe07e
Your Name <you@example.com>
2022-07-27 10:08:56 -0500

    Proyecto inicializado

---
+++ app/Models/Continent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Continent extends Model
+{
+    //La tabla a conectar:
+    protected $table = "continents";
+    //La clave primaria de esa tabla:
+    protected $primaryKey = "continent_id";
+    //Omitir campos de auditoria:
+    public $timestamps =false;
+    use HasFactory;
+}
